fb418c3888149cce567c4f8cd6f34178b2e0e622
Unknown <t.brussaard@students.uu.nl>
2018-12-07 23:30:51 +0100

    Everything but ex 11 - 12

--- src/StartingFramework.hs
+++ src/StartingFramework.hs
@@ -2,12 +2,14 @@
 module Main where
 
 import Prelude hiding ((<*>),(*>),(<*),(<$>),(<$),($>))
-import Data.List (intercalate, find, permutations)
-import Data.Maybe (fromJust)
+import Data.List (intercalate, find, permutations, concatMap, unzip, sort, nubBy)
+import Data.Text (stripEnd, pack, unpack)
+import Data.Maybe (fromJust, isNothing)
 
-import ParseLib.Abstract
+import ParseLib.Abstract hiding (pack)
 import System.Environment
 import System.IO
+import qualified Data.Time.Calendar as C (fromGregorian, diffDays, Day(..))
 import Data.DeriveTH (derive, makeIs)
 
 -- Starting Framework
@@ -52,8 +54,6 @@ printDateTime x = yearx ++ monthx ++ dayx ++ ['T'] ++ hourx ++ minutex ++ second
                             True -> ['Z']
                             False -> []
 
---------- Splice -----------
-
 data Token = 
     BEv
   | EEv
@@ -70,20 +70,10 @@ data Token =
   | Version String
   deriving (Eq, Ord, Show) --{-! Is !-}
 
-makeEvent :: [Token] -> Maybe Event
-makeEvent tks  = do
-    case x@(tks !! 0) isStamp of
-        False   -> Nothing
-        True    -> let st = fromToken x
-    case y@(tks !! 1) isUID of
-        False -> Nothing
-        True -> let uid = fromToken y
-    where
-        fromToken _ a = a
+--------- Splice -----------
 
 -- Derive magic
 $( derive makeIs ''Token )
-
 
 -- | The main interaction function. Used for IO, do not edit.
 data Result = SyntaxError | Invalid DateTime | Valid DateTime deriving (Eq, Ord)
@@ -93,9 +83,17 @@ instance Show Result where
     show (Invalid _) = "good syntax, but invalid date or time values"
     show (Valid x)   = "valid date: " ++ show x
 
+    -- Test Data
+    --"BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T170000Z\r\nDTEND:19970714T190000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:19970610T172345Z-AF23B2@example.com\r\nDTSTAMP:19970610T172345Z\r\nDTSTART:19970714T180000Z\r\nDTEND:19970715T200000Z\r\nSUMMARY:Bastille Day Party\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n"
+    --[BEv, UID "19970610T172345Z-AF23B2@example.com", Stamp x, DTS x, DTE x]
+    --where x = DateTime {date = Date{year=Year 1998,month= Month 10,day= Day 8},time = Time{hour= Hour 6, minute= Minute 34, second= Second 55},utc= True}
 main :: IO ()
---main = putStrLn $ show $ fromJust $ run parseDateTime "19970610T172345Z"
---main = putStrLn $ unlines $ map show $ fromJust $ run scanCalendar " DTSTAMP:19970610T172345Z    "
+main = do
+    file:_  <- getArgs
+    res     <- readCalendar file
+    putStrLn $ show res
+    putStrLn $ maybe "Calendar parsing error" (show) res
+{-
 main = putStrLn $ show . setupCalendar . concat $ map (fromJust . run scanCalendar)
         [
             "BEGIN:VCALENDAR ",
@@ -110,7 +108,7 @@ main = putStrLn $ show . setupCalendar . concat $ map (fromJust . run scanCalend
             "END:VEVENT",
             " END:VCALENDAR"
         ]
-
+-}
 
 mainDateTime :: IO ()
 mainDateTime = interact (printOutput . processCheck . processInput)
@@ -118,22 +116,26 @@ mainDateTime = interact (printOutput . processCheck . processInput)
         processInput = map (run parseDateTime) . lines
         processCheck = map (maybe SyntaxError (\x -> if checkDateTime x then Valid x else Invalid x))
         printOutput  = unlines . map show
-
+{-
 mainCalendar :: IO ()
 mainCalendar = do
     file:_ <- getArgs
     res <- readCalendar file
     putStrLn $ maybe "Calendar parsing error" (ppMonth (Year 2012) (Month 11)) res
+-} --TODO
 
 -- Exercise 1
 parseDateTime :: Parser Char DateTime
 parseDateTime = (\x _ y z -> DateTime x y z) <$> dateP <*> anySymbol <*> timeP <*> utcP 
 
--- Exercise 2  --TODO: check if parse p x is an empty list first
+-- Exercise 2
 run :: Parser a b -> [a] -> Maybe b
-run p x = case head (parse p x) of 
-        (xs,[]) -> Just xs
-        _       -> Nothing
+run p x =   let res = parse p x
+            in  case res of
+                    []  -> Nothing
+                    _   -> case head res of 
+                            (xs,[]) -> Just xs
+                            _       -> Nothing
 
 -- Exercise 3
 --Due to the splice, this has been moved to line 41
@@ -155,8 +157,6 @@ checkDateTime x     | monthBool && dayBool && hourBool && minuteBool && secondBo
                     hourBool = checkHour $ unHour $ hour $ time x
                     minuteBool = checkMinute $ unMinute $ minute $ time x
                     secondBool = checkSecond $ unSecond $ second $ time x
-
-
 
 -- help functions
 checkMonth :: Int -> Bool
@@ -191,8 +191,6 @@ checkSecond :: Int -> Bool
 checkSecond x   | x <= 59 && x >= 0 = True
                 | otherwise = False
 
-
-
 -- Exercise 6
 data Calendar = Calendar { prodid :: String
                          , version :: String
@@ -200,25 +198,28 @@ data Calendar = Calendar { prodid :: String
     deriving (Eq, Ord)
 
 instance Show Calendar where
-    show cal@{prodid = id, version = ver, event = evs} =
+    show Calendar{prodid = id, version = ver, event = evs} =
                "BEGIN:VCALENDAR\r\n"
             ++ "PRODUCT_ID:" ++ id ++ "\r\n"
             ++ "VERSION:" ++ ver ++ "\r\n"
-            ++ (map showEv evs)
+            ++ (concatMap showEv evs)
             ++ "END:VCALENDAR"
         where
-            showEv ev@{dtstamp = stamp, uid = uniqId, dtstart = dtS, dtend = dtE, descripion = des, summary = sum, location = loc} =
+            showEv Event{dtstamp = stamp, uid = uniqId, dtstart = dtS, dtend = dtE, description = des, summary = sum, location = loc} =
                        "BEGIN:VEVENT\r\n"
-                    ++ "UID:" ++ uniqId + "\r\n"
+                    ++ "UID:" ++ uniqId ++ "\r\n"
                     ++ "DTSTAMP:" ++ (printDateTime stamp) ++ "\r\n"
-                    ++ "DTSTART:" ++ (printDateTime dts) ++ "\r\n"
-                    ++ "DTEND:" ++ (printDateTime dte) ++ "\r\n"
-                    ++ 
+                    ++ "DTSTART:" ++ (printDateTime dtS) ++ "\r\n"
+                    ++ "DTEND:" ++ (printDateTime dtE) ++ "\r\n"
+                    ++ (concatMap checkEmpty [(des,"DESCRIPTION:"), (sum,"SUMMARY:"), (loc,"LOCATION:")])
                     ++ "END:VEVENT\r\n"
-            where 
-                checkEmpty () = 
+            checkEmpty (Nothing, _) = ""
+            checkEmpty (Just v,s)   = s ++ v ++ "\r\n"
 
-
+cheekyCheck :: String -> String
+cheekyCheck  = reverse . f . reverse
+                where f ('\n':'\r':xs) = xs
+                      f s              = s
 
 data Event = Event  { dtstamp :: DateTime 
                     , uid :: String
@@ -263,8 +264,8 @@ pSep = True <$ token "\r\n"
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = parseCalendar1 <|> parseCalendar2
-parseCalendar1 = (\x y -> Calendar y x []) <$ parseBCal <*> parseVersion <*> parseProdID <* eof
-parseCalendar2 = (\x y -> Calendar x y []) <$ parseBCal <*> parseProdID <*> parseVersion <* eof
+parseCalendar1 = (\x y z -> Calendar y x z) <$ parseBCal <*> parseVersion <*> parseProdID <*> parseEvents <* parseECal
+parseCalendar2 = (\x y z -> Calendar x y z) <$ parseBCal <*> parseProdID <*> parseVersion <*> parseEvents <* parseECal
 
 {-
 parsen tot beginevent of endcalender
@@ -280,32 +281,57 @@ recognizeCalendar s = run scanCalendar s >>= run parseCalendar
 -- Exercise 8
 readCalendar :: FilePath -> IO (Maybe Calendar)
 readCalendar s = do
-    h <- openFile ReadMode s
-    hSetNewLineMode noNewLineTranslation
-    cont <- seq $ hGetContents h
-    {-
-    let ls      = lines cont
-    let calRaw  = intersperse "\r\n" ls
-    -}
-    return $ recognizeCalendar cont --calRaw
+    h <- openFile s ReadMode
+    hSetNewlineMode h noNewlineTranslation
+    cont <- hGetContents h
+
+    let cont' = cheekyCheck cont
+    let res = recognizeCalendar cont'
+    res `seq` (hClose h)
+    return res
 
 -- Exercise 9
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
 printCalendar :: Calendar -> String
-printCalendar = show
+printCalendar = show    -- for implementation see instace Show of Calendar
 
 -- Exercise 10
 countEvents :: Calendar -> Int
-countEvents = undefined
+countEvents = length . event
 
 findEvents :: DateTime -> Calendar -> [Event]
-findEvents = undefined
+findEvents t =  let res = filter (\e -> (dtstart e >= t) && (t <= dtend e) ) . event
+                in res
 
 checkOverlapping :: Calendar -> Bool
-checkOverlapping = undefined
+checkOverlapping = ((/=0) . length) . nubBy check_overlap . (\(x,y) -> zip x y) . (\(a,b) -> (sort a, sort b)) . unzip . map (\e -> (dtstart e, dtend e)) . event
+    where check_overlap (_,b) (c,_) = c < b
 
 timeSpent :: String -> Calendar -> Int
-timeSpent = undefined
+timeSpent s c = res c
+        where
+            res         = count . map (\e -> (dtstart e, dtend e)) . filter ((s==) . fromJust . summary) . event
+            count       = case checkOverlapping c of
+                            False   -> sum . map (\(a,b) -> a -$- b ) 
+                            True    -> sum . (\(a,b) (c,d) -> )  . (\(x,y) -> zip x y) . (\(a,b) -> (sort a, sort b)) . unzip
+
+(-$-) :: DateTime -> DateTime -> Int
+(-$-) DateTime{ date = date1, time = time1 } DateTime{ date = date2, time = time2 }
+        = (fromInteger diffD) * 1440 + (h2 - h1) * 60 + (m2 - m1) + (s2 - s1) `div` 60
+    where
+            diffD       = C.diffDays (C.fromGregorian (toInteger y1) m1 d1) (C.fromGregorian (toInteger y2) m2 d2)
+            y1      = unYear    $   year   date1
+            m1      = unMonth   $   month  date1
+            d1      = unDay     $   day    date1
+            h1      = unHour    $   hour   time1
+            min1    = unMinute  $   minute time2
+            s1      = unSecond  $   second time2
+            y2      = unYear    $   year   date2
+            m2      = unMonth   $   month  date2
+            d2      = unDay     $   day    date2
+            h2      = unHour    $   hour   time2
+            min2    = unMinute  $   minute time2
+            s2      = unSecond  $   second time2
 
 -- Exercise 11
 ppMonth :: Year -> Month -> Calendar -> String
@@ -382,39 +408,11 @@ pLocation :: Parser Char Token
 pLocation       = (<$>) LOC $ token "LOCATION:" *> pString_ <* pSpaces
 
 pString_ :: Parser Char String
-pString_        = some anySymbol
-
-pVerNum :: Parser Char Float
-pVerNum = read <$> some anySymbol <* pSpaces
+pString_ = some $ satisfy (/= '\r')
 
 pSpaces :: Parser Char String
 pSpaces         = many $ satisfy (\c -> c == ' ')
 
--- No easy way to automatically generate a list of all the derived functions
-
-mandatoryTokens :: [Token] -> Bool
-mandatoryTokens = (one_s==) . flip map (count `fmap` is_s) . flip ($)
-            where
-                count f = length . filter f
-                is_s  = [isBEv, isECal, isProdID, isVersion]
-                one_s = replicate (length is_s)  1
-
-{-
-tokenRestraints :: [Token] -> Bool
-tokenRestraints = flip map (count `fmap` is_s) . flip ($)
-    where count f = length . filter f
-          is_s  = [isBEv, isEEv, isStamp, isUID, isDTS, isDTE, isDES, isSUM, isLOC, isBCal, isECal, isProdID, isVersion]
--}
-
-setupCalendar :: [Token] -> Maybe Calendar
-setupCalendar tks = let events = []
-    in do
-        let pId = find isProdID tks
-        let v   = find isVersion tks
-        case (pId, v) of
-            (Nothing, _)                                -> Nothing
-            (_, Nothing)                                -> Nothing
-            ( Just (ProdID pId_), Just (Version v_) )   -> Just Calendar {prodid= pId_, version = v_, event=events}
 
 parseBCal :: Parser Token Token
 parseBCal = satisfy (\x -> x == BCal)
@@ -475,8 +473,8 @@ parseEEvent :: Parser Token Token
 parseEEvent = satisfy (\x -> x == EEv)
 
 parseAllEvent :: Parser Token Token
-parseAllEvent = satisfy isStamp2 <|> satisfy isUID2 <|> satisfy isDTS2 <|> satisfy isDTE2 <|> satisfy isDES2 <|> satisfy isSUM2 <|> satisfy isLOC2
-
+parseAllEvent = satisfy isStamp <|> satisfy isUID <|> satisfy isDTS <|> satisfy isDTE <|> satisfy isDES <|> satisfy isSUM <|> satisfy isLOC
+{-
 isStamp2 :: Token -> Bool
 isStamp2 (Stamp _) = True
 isStamp2 _         = False
@@ -504,3 +502,31 @@ isSUM2 _         = False
 isLOC2 :: Token -> Bool
 isLOC2 (LOC _) = True
 isLOC2 _         = False
+-}
+-- Legacy Code
+
+-- No easy way to automatically generate a list of all the derived functions
+
+mandatoryTokens :: [Token] -> Bool
+mandatoryTokens = (one_s==) . flip map (count `fmap` is_s) . flip ($)
+            where
+                count f = length . filter f
+                is_s  = [isBEv, isECal, isProdID, isVersion]
+                one_s = replicate (length is_s)  1
+
+{-
+tokenRestraints :: [Token] -> Bool
+tokenRestraints = flip map (count `fmap` is_s) . flip ($)
+    where count f = length . filter f
+          is_s  = [isBEv, isEEv, isStamp, isUID, isDTS, isDTE, isDES, isSUM, isLOC, isBCal, isECal, isProdID, isVersion]
+-}
+
+setupCalendar :: [Token] -> Maybe Calendar
+setupCalendar tks = let events = []
+    in do
+        let pId = find isProdID tks
+        let v   = find isVersion tks
+        case (pId, v) of
+            (Nothing, _)                                -> Nothing
+            (_, Nothing)                                -> Nothing
+            ( Just (ProdID pId_), Just (Version v_) )   -> Just Calendar {prodid= pId_, version = v_, event=events}
